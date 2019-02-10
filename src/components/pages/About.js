d0cee94187299aa2adcf93bc3a849e1ae2d76e1b
lauraerwin <43740937+lauraerwin@users.noreply.github.com>
2019-02-10 23:21:34 +0100

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+function About() {
+    return (
+      <React.Fragment>
+
+          <h1>This is the To do list app v1.0.0. </h1>
+        
+      </React.Fragment>
+    )
+  }
+
+
+export default About;
